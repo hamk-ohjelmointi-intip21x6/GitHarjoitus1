c5ddc92497ae9485fe7fc7aff176f502780cedda
Huseyin <Huseyin@DESKTOP-9IRRJ3P>
2021-09-16 13:28:41 +0300

    Add project files.

---
+++ GitHarjoitus2021/Laskin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHarjoitus2021
+{
+    public class Laskin
+    {
+        public int Erotus(int luku1, int luku2)
+        {
+            return luku1 - luku2;
+        }
+        public int Summa(int luku1, int luku2)
+        {
+            return luku1 + luku2;
+        }
+        public int JakoJaannos(int luku1, int luku2)
+        {
+            return luku1 % luku2;
+        }
+    }
+}
+
+    
---
+++ GitHarjoitus2021/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GitHarjoitus2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Terve Laskin!");
+
+            Laskin laskimeni = new Laskin();
+
+            Console.WriteLine("Laske lukujen 148 ja 56 erotus");
+            int erotus = laskimeni.Erotus(148, 56);
+            Console.WriteLine("erotus on: " + erotus);
+
+            Console.WriteLine("Laske lukujen 3005 ja 453 summa");
+            int summa = laskimeni.Summa(3005, 453);
+            Console.WriteLine("summa on: " + summa);
+
+            Console.WriteLine("Laske lukujen 15 ja 4 jakojäännös");
+            int jakojaannos = laskimeni.JakoJaannos(15, 4);
+            Console.WriteLine("Jakojäännös on: " + jakojaannos);
+        }
+
+    }
+}
